5a8d031dd2f711ff9f3a4f8447db3a42feb201c1
feverK13 <ihor.k2006@gmail.com>
2026-03-31 14:13:05 +0300

    completed second task and added readme file

---
+++ task 2/success.js
@@ -0,0 +1,14 @@
+const savedParams = localStorage.getItem("myParams");
+
+//теж базовий запобіжник
+if (savedParams) {
+  const params = new URLSearchParams(savedParams);
+  const source = params.get("utm_source");
+
+  //тут на відміну від першого завдання використав махінацію з рядками,
+  // тому що тут мені потрібно було дістати конкретне значення, а там весь source набір параметірв
+  const postbackUrl = `https://tracker.com/postback?status=lead&source=${source}`;
+
+  console.log("Відправляємо Postback на:", postbackUrl);
+  fetch(postbackUrl, { mode: "no-cors" }); // no-cors щоб провести відрправку без лишніх мені відповідей
+}
---
+++ task 2/task2.js
@@ -0,0 +1,12 @@
+const urlSearch = window.location.search;
+const currentParamsElem = document.getElementById("current-params");
+currentParamsElem.textContent = urlSearch;
+
+//базовий запобіжник щоб не зберігався пустий рядок
+if (urlSearch) {
+  currentParamsElem.textContent = urlSearch;
+  localStorage.setItem("myParams", urlSearch);
+} else {
+  currentParamsElem.textContent =
+    localStorage.getItem("myParams") || "Параметри відсутні";
+}
