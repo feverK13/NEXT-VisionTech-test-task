b4b9f3a72916e951607ab56ca17de0cd38138878
feverK13 <ihor.k2006@gmail.com>
2026-04-09 13:09:32 +0300

    new commit

--- task 2/success.js
+++ task 2/success.js
@@ -1,14 +1,31 @@
-const savedParams = localStorage.getItem("myParams");
+// const savedParams = localStorage.getItem("myParams");
 
-//теж базовий запобіжник
-if (savedParams) {
-  const params = new URLSearchParams(savedParams);
-  const source = params.get("utm_source");
+// //теж базовий запобіжник
+// if (savedParams) {
+//   const params = new URLSearchParams(savedParams);
+//   const source = params.get("utm_source");
 
-  //тут на відміну від першого завдання використав махінацію з рядками,
-  // тому що тут мені потрібно було дістати конкретне значення, а там весь source набір параметірв
-  const postbackUrl = `https://tracker.com/postback?status=lead&source=${source}`;
+//   //тут на відміну від першого завдання використав махінацію з рядками,
+//   // тому що тут мені потрібно було дістати конкретне значення, а там весь source набір параметірв
+//   const postbackUrl = `https://tracker.com/postback?status=lead&source=${source}`;
 
-  // console.log("Відправляємо Postback на:", postbackUrl);
-  // fetch(postbackUrl, { mode: "no-cors" }); // no-cors щоб провести відрправку без лишніх мені відповідей
-}
+//   console.log("Відправляємо Postback на:", postbackUrl);
+//   fetch(postbackUrl, { mode: "no-cors" }); // no-cors щоб провести відрправку без лишніх мені відповідей
+// }
+
+const clickflarePixelUrl =
+  "https://globalvisitlog.com/cf/cv?click_id=OPTIONAL&payout=OPTIONAL&txid=OPTIONAL";
+
+console.log("Відправляємо конверсію на:", clickflarePixelUrl);
+
+// Відправляємо запит
+fetch(clickflarePixelUrl, {
+  method: "GET",
+  mode: "no-cors", // Цей параметр ігнорує політику блокування відповідей
+})
+  .then(() => {
+    console.log("Конверсія успішно відправлена в Clickflare!");
+  })
+  .catch((err) => {
+    console.error("Помилка відправки:", err);
+  });
